265ff0275e52666aa98171dfd12d3baa3803e430
xuxiaoming <1229717912@qq.com>
2019-10-22 15:25:50 +0800

    添加Api接口和图片资源

---
+++ app/src/main/java/com/xxm/wanandroid/http/Api.java
@@ -0,0 +1,63 @@
+package com.xxm.wanandroid.http;
+
+/**
+ * Api接口
+ */
+public class Api {
+
+    public final static String HOME_BANNER = "https://www.wanandroid.com/banner/json";
+
+    public final static String HOME_ARTICLE_LIST = "https://www.wanandroid.com/article/list/";
+    /// 知识体系
+    public final static String SYSTEM_TREE = "https://www.wanandroid.com/tree/json";
+    // 知识体系详情
+    public final static String SYSTEM_TREE_CONTENT = "https://www.wanandroid.com/article/list/";
+    // 公众号名称
+    public final static String WX_LIST = "https://wanandroid.com/wxarticle/chapters/json";
+    // 公众号文章
+    public final static String WX_ARTICLE_LIST = "https://wanandroid.com/wxarticle/list/";
+    // 导航列表数据
+    public final static String NAVI_LIST = "https://www.wanandroid.com/navi/json";
+    // 项目分类
+    public final static String PROJECT_TREE = "https://www.wanandroid.com/project/tree/json";
+    // 项目列表
+    public final static String PROJECT_LIST = "https://www.wanandroid.com/project/list/";
+    // 搜索热词
+    public final static String SEARCH_HOT_WORD = "https://www.wanandroid.com//hotkey/json";
+    // 搜索结果
+    public final static String SEARCH_RESULT = "https://www.wanandroid.com/article/query/";
+    // 用户登录
+    public final static String USER_LOGIN = "https://www.wanandroid.com/user/login";
+    // 用户注册
+    public final static String USER_REGISTER = "https://www.wanandroid.com/user/register";
+    // 收藏列表
+    public final static String COLLECTION_LIST = "https://www.wanandroid.com/lg/collect/list/";
+    //常用网站
+    public final static String COMMON_WEBSITE = "https://www.wanandroid.com/friend/json";
+
+
+    // 我的收藏-取消收藏
+    public final static String CANCEL_COLLECTION = "https://www.wanandroid.com/lg/uncollect/";
+
+    // 我的收藏-新增收藏
+    public final static String ADD_COLLECTION = "https://www.wanandroid.com/lg/collect/add/json";
+    // 网站收藏
+    public final static String WEBSITE_COLLECTION_LIST = "https://www.wanandroid.com/lg/collect/usertools/json";
+    // 取消网站收藏
+    public final static String CANCEL_WEBSITE_COLLECTION = "https://www.wanandroid.com/lg/collect/deletetool/json";
+    // 新增网站收藏
+    public final static String ADD_WEBSITE_COLLECTION = "https://www.wanandroid.com/lg/collect/addtool/json";
+    // 编辑网站收藏
+    public final static String EDIT_WEBSITE_COLLECTION = "https://www.wanandroid.com/lg/collect/updatetool/json";
+
+    // todo列表数据
+    public final static String TODO_LIST = "https://wanandroid.com/lg/todo/list/";
+    // 新增todo数据
+    public final static String ADD_TODO = "https://www.wanandroid.com/lg/todo/add/json";
+    // 更新todo数据
+    public final static String UPDATE_TODO = "https://www.wanandroid.com/lg/todo/update/";
+    // 删除todo数据
+    public final static String DELETE_TODO = "https://www.wanandroid.com/lg/todo/delete/";
+    // 仅更新todo完成状态
+    public final static String DONE_TODO = "https://www.wanandroid.com/lg/todo/done/";
+}
